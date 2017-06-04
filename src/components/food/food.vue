48051c5c6af391341f2298980875f8ebbd092a84
PeterCheng1 <1284995325@qq.com>
2017-06-05 01:28:14 +0800

    ratingSelect-components

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,6 +1,7 @@
 <template>
   <transition  name="move">
     <div v-show="showFlag" class="food" ref="food">
+      <div class="food-content">
         <div class="image-header">
             <img :src="food.image" alt="">
             <div class="back" @click="hide">
@@ -32,16 +33,23 @@
         <split></split>
         <div class="rating">
             <h1 class="title">商品评价</h1>
+            <ratingselect :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
         </div>
+      </div>  
     </div>
   </transition >
 </template>
 
 <script>
-  import BScroll from 'better-scroll';  
+  import BScroll from 'better-scroll';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
+  import ratingselect from 'components/ratingselect/ratingselect'
   import split from 'components/split/split'
   import Vue from 'vue';
+
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
+  const ALL = 2;
 
   export default {
     props:{
@@ -51,19 +59,30 @@
     },
     data () {
         return {
-            showFlag:false
+            showFlag:false,
+            selectType:ALL,
+            onlyContent:true,
+            desc:{
+                all:'全部',
+                positive:'推荐',
+                negative:'吐槽'
+            }
         }
     },
     methods : {
         show () {
             this.showFlag = true;
-            this.$nextTick(()=>{
-                if(!this.scroll){
-                    this.scroll = new BScroll(this.$refs.food,{
-                        click:true
+            this.selectType = ALL;
+            this.onlyContent = true;
+            this.$nextTick(() => {
+                if (!this.scroll) {
+                    console.log(this.$refs.food)
+                    this.scroll = new BScroll(this.$refs.food, {
+                    click: true
                     });
-                }else{
-                   this.scroll.refresh();
+                } else {
+                    console.log(this.scroll)
+                    this.scroll.refresh();
                 }
             })
         },
@@ -91,7 +110,8 @@
     },
     components :{
         cartcontrol,
-        split
+        split,
+        ratingselect
     }
   };
 </script>
@@ -105,115 +125,127 @@
         z-index: 30;
         width: 100%;
         background: #fff;
-        transform:translate3d(0%,0,0);
+        transform: translate3d(0, 0, 0);
         &.move-enter-active, &.move-leave-active{
             transition: all .2s linear;
         }
         &.move-enter, &.move-leave-active{
             transform: translate3d(100%, 0, 0); 
         }
-        .image-header{
-            position:relative;
-            width:100%;
-            height:0;
-            padding-top:100%;
-            img{
-                position:absolute;
-                top:0;
-                left:0;
+            .image-header{
+                position:relative;
                 width:100%;
-                height:100%;
-            }
-            .back{
-                position:absolute;
-                top:10px;
-                left:0;
-                .icon-arrow_lift{
-                    display:block;
-                    padding:10px;
-                    font-size:20px;
-                    color:#fff;
+                height:0;
+                padding-top:100%;
+                img{
+                    position:absolute;
+                    top:0;
+                    left:0;
+                    width:100%;
+                    height:100%;
+                }
+                .back{
+                    position:absolute;
+                    top:10px;
+                    left:0;
+                    .icon-arrow_lift{
+                        display:block;
+                        padding:10px;
+                        font-size:20px;
+                        color:#fff;
+                    }
                 }
             }
-        }
-        .content{
-            padding:18px;
-            .title{
-                line-height:14px;
-                margin-bottom:8px;
-                font-size:14px;
-                font-weight:700;
-                color:rgb(7,17,27);
-            }
-            .detail{
-                margin-bottom:18px;
-                line-height:10px;
-                height:10px;
-                font-size:0;
-            }
-            .sell-count,.rating{
-                font-size:10px;
-                color:rgb(147,153,159);
-            }
-            .sell-count{
-                margin-right:12px;
-            }
-            .price{
-                font-weight:700;
-                line-height:24px;
-                .now{
-                    margin-right:8px;
+            .content{
+                position: relative;
+                padding: 18px;
+                .title{
+                    line-height:14px;
+                    margin-bottom:8px;
                     font-size:14px;
-                    color:rgb(240,20,20);
+                    font-weight:700;
+                    color:rgb(7,17,27);
                 }
-                .old{
-                    text-decoration:line-through;
+                .detail{
+                    margin-bottom:18px;
+                    line-height:10px;
+                    height:10px;
+                    font-size:0;
+                }
+                .sell-count,.rating{
                     font-size:10px;
                     color:rgb(147,153,159);
                 }
+                .sell-count{
+                    margin-right:12px;
+                }
+                .price{
+                    font-weight:700;
+                    line-height:24px;
+                    .now{
+                        margin-right:8px;
+                        font-size:14px;
+                        color:rgb(240,20,20);
+                    }
+                    .old{
+                        text-decoration:line-through;
+                        font-size:10px;
+                        color:rgb(147,153,159);
+                    }
+                }
+                .cartcontrol-warpper{
+                    position: absolute;
+                    right: 12px;
+                    bottom: 12px;
+                }
+                .buy{
+                    position: absolute;
+                    right: 18px;
+                    bottom: 18px;
+                    z-index: 10;
+                    height: 24px;
+                    line-height: 24px;
+                    padding: 0 12px;
+                    box-sizing: border-box;
+                    border-radius: 12px;
+                    font-size: 10px;
+                    color: #fff;
+                    background: rgb(0, 160, 220);
+                    opacity: 1;
+                }
+                .fade-enter-active,.fade-leave-active{
+                    transition: all .2s
+                }
+                .fade-enter,.fade-leave-active{
+                    opacity:0;
+                }                        
             }
-            .cartcontrol-warpper{
-                position:absolute;
-                right:12px;
-                bottom:146px;
-            }
-            .buy{
-                position:absolute;
-                right:18px;
-                bottom:152px;
-                z-index:10;
-                height:24px;
-                line-height:24px;
-                padding:0 12px;
-                box-sizing:border-box;
-                font-size:10px;
-                border-radius:12px;
-                color:#fff;
-                background:rgb(0,160,220);
-            }
-            .fade-enter-active,.fade-leave-active{
-                transition: all .2s
-            }
-            .fade-enter,.fade-leave-active{
-                opacity:0;
-            }                        
-        }
-        .info {
-            padding:18px;
-            .title{
-                line-height:14px;
-                margin-bottom:6px;
-                font-size:14px;
-                color:rgb(7,17,27);
-                font-weight: 700;
-            }
-            .text{
-                line-height:24px;
-                padding:0 8px;
-                font-size:12px;
-                color:rgb(77,85,93);
-            }
+            .info {
+                padding:18px;
+                .title{
+                    line-height:14px;
+                    margin-bottom:6px;
+                    font-size:14px;
+                    color:rgb(7,17,27);
+                    font-weight: 700;
+                }
+                .text{
+                    line-height:24px;
+                    padding:0 8px;
+                    font-size:12px;
+                    color:rgb(77,85,93);
+                }
 
-        }
+            }
+            .rating{
+                padding-top:18px;
+                .title{
+                    line-height:14px;
+                    margin-left:18px;
+                    font-size:14px;
+                    color:rgb(7,17,27);
+                    font-weight: 700;
+                }
+            }            
     }
 </style>
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,16 +1,144 @@
 <template>
-  <div>
-   
+  <div class="ratingselect">
+    <div class="rating-type border-1px">
+      <span class="block positive" :class="{'active':selectType===2}" @click="select(2,$event)">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+      <span class="block positive" :class="{'active':selectType===0}" @click="select(0,$event)">{{desc.positive}}<span class="count">{{positive.length}}</span></span>
+      <span class="block negative" :class="{'active':selectType===1}" @click="select(1,$event)">{{desc.negative}}<span class="count">{{negative.length}}</span></span>
+    </div>
+    <div class="switch" :class="{'on':onlyContent}" @click="toggleContent($event)">
+      <span class="icon-check_circle"></span>
+      <span class="text">只看有内容的评价</span>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
+  const ALL = 2;
 
+  export default {
+    props : {
+      ratings:{
+        type:Array,
+        default () {
+          return [];
+        }
+      },//评论数
+      selectType :{
+        type :Number,
+        default : ALL //默认显示全部  全部 推荐 吐槽 或者 全部 满意 不满意
+      },//选择需要显示的评论类型
+      onlyContent : {
+        type : Boolean,
+        default : false
+      },
+      desc : {
+        type : Object,
+        default () {
+          return {
+            all : '全部',
+            positive : '满意',
+            negative : '不满意'
+          };
+        }
+      }
+    },
+    computed:{
+      positive() {
+        return this.ratings.filter(rating=>{
+          return rating.rateType === POSITIVE;
+        })
+      },
+      negative() {
+        return this.ratings.filter(rating=>{
+          return rating.rateType === NEGATIVE;
+        })
+      }
+    },
+    data () {
+      return {}
+    },
+    methods : {
+      select(type,event){
+        if(!event._constructed){
+          return;
+        }
+        this.selectType = type;
+        this.$emit('ratingtype.select',type);
+      },
+      toggleContent (event) {
+        if(!event._constructed){
+          return;
+        }
+        this.onlyContent = !this.onlyContent;
+        this.$emit('content.toggle',this.onlyContent);
+      }
+    }
   }
 
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
- 
+  @import '../../common/scss/mixin.scss';
+
+    .ratingselect{
+      .rating-type{
+        padding:18px 0;
+        margin: 0 18px;
+        @include border-1px(rgba(7,17,27,0.1));
+        font-size:0;
+        .block{
+          display:inline-block;
+          padding:8px 12px;
+          margin-right:8px;
+          border-radius:2px;
+          font-size:12px;
+          line-height:16px;
+          color:rgb(77,75,93);
+          &.active{
+            color:#fff;
+          }
+          .count{
+            margin-left:2px;
+            font-size:8px;
+          }
+          &.positive{
+            background:rgba(0,160,220,0.2);
+            &.active{
+              background:rgba(0,160,220,1);
+            }
+          }
+          &.negative{
+            background:rgba(77,85,93,0.2);
+            &.active{
+              background:rgba(77,85,93,1);
+            }
+          }
+        }
+      }
+      .switch{
+        padding:12px 18px;
+        line-height:24px;
+        border-bottom:1px solid rgba(7,17,27,0.1);
+        color:rgb(147,153,159);
+        font-size:0;
+        &.on{
+          .icon-check_circle{
+            color:#00c850;
+          }
+        }
+        .icon-check_circle{
+          display:inline-block;
+          vertical-align:top;
+          margin-right:4px;
+          font-size:24px;
+        }
+        .text{
+          display:inline-block;
+          vertical-align:top;
+          font-size:12px;
+        }
+      }
+    }
 </style>
